1bdd2ead2179a7054fe3e3af690f557687cadbb7
Tectonik <marto7395@gmail.com>
2015-09-19 07:29:40 +0300

    Moved game workings from main to new class

--- Battle-Field-1/BattleFieldGame.cs
+++ Battle-Field-1/BattleFieldGame.cs
@@ -7,71 +7,10 @@
 
     public class BattleFieldGame
     {
-        private const int MinBattleFieldSize = 1;
-        private const int MaxBattleFieldSize = 10;
-
         public static void Main(string[] argumenti)
         {
-            Console.Write("Welcome to \"Battle Field game.\" Enter battle field size: n = ");
-            int battleFieldSize = Convert.ToInt32(Console.ReadLine());
-            while (battleFieldSize < MinBattleFieldSize || battleFieldSize > MaxBattleFieldSize)
-            {
-                Console.WriteLine("Enter a number between {0} and {1}!", MinBattleFieldSize, MaxBattleFieldSize);
-                battleFieldSize = Convert.ToInt32(Console.ReadLine());
-            }
-
-            int fieldHeight = battleFieldSize + 2;
-            int fieldWidth = (battleFieldSize * 2) + 2;
-
-            string[,] battleField = new string[fieldHeight, fieldWidth];
-
-            battleField[0, 0] = " ";
-            battleField[0, 1] = " ";
-            battleField[1, 0] = " ";
-            battleField[1, 1] = " ";
-
-            for (int row = 2; row < fieldHeight; row++)
-            {
-                for (int col = 2; col < fieldWidth; col++)
-                {
-                    if (col % 2 == 0)
-                    {
-                        if (col == 2)
-                        {
-                            battleField[0, col] = "0";
-                        }
-                        else
-                        {
-                            battleField[0, col] = Convert.ToString((col - 2) / 2);
-                        }
-                    }
-                    else
-                    {
-                        battleField[0, col] = " ";
-                    }
-
-                    if (col < fieldWidth - 1)
-                    {
-                        battleField[1, col] = "-";
-                    }
-
-                    battleField[row, 0] = Convert.ToString(row - 2);
-                    battleField[row, 1] = "|";
-                    if (col % 2 == 0)
-                    {
-                        battleField[row, col] = "-";
-                    }
-                    else
-                    {
-                        battleField[row, col] = " ";
-                    }
-                }
-            }
-
-            Methods.FillingTheArray(battleFieldSize, fieldHeight, fieldWidth, battleField);
-            Methods.PrintArray(fieldHeight, fieldWidth, battleField);
-            int countPlayed = 0;
-            Methods.VremeEIgrachaDaDeistva(battleFieldSize, fieldHeight, fieldWidth, battleField, countPlayed);
+            var game = new BattlefieldGameEngine();
+            game.Run();
         }
     }
 }
--- Battle-Field-1/Methods.cs
+++ Battle-Field-1/Methods.cs
@@ -1,12 +1,78 @@
 ﻿namespace BattleField
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
 
-    public class Methods
+    public class BattlefieldGameEngine
     {
+        private const int MinBattleFieldSize = 1;
+        private const int MaxBattleFieldSize = 10;
+        private const int BorderSize = 2;
+
+        public void Run()
+        {
+            Console.Write("Welcome to \"Battle Field game.\" Enter battle field size: n = ");
+            int battleFieldSize = Convert.ToInt32(Console.ReadLine());
+            while (battleFieldSize < MinBattleFieldSize || battleFieldSize > MaxBattleFieldSize)
+            {
+                Console.WriteLine("Enter a number between {0} and {1}!", MinBattleFieldSize, MaxBattleFieldSize);
+                battleFieldSize = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int fieldHeight = battleFieldSize + BorderSize;
+            int fieldWidth = (battleFieldSize * 2) + BorderSize;
+
+            string[,] battleField = new string[fieldHeight, fieldWidth];
+
+            battleField[0, 0] = " ";
+            battleField[0, 1] = " ";
+            battleField[1, 0] = " ";
+            battleField[1, 1] = " ";
+
+            for (int row = 2; row < fieldHeight; row++)
+            {
+                for (int col = 2; col < fieldWidth; col++)
+                {
+                    if (col % 2 == 0)
+                    {
+                        if (col == 2)
+                        {
+                            battleField[0, col] = "0";
+                        }
+                        else
+                        {
+                            battleField[0, col] = Convert.ToString((col - 2) / 2);
+                        }
+                    }
+                    else
+                    {
+                        battleField[0, col] = " ";
+                    }
+
+                    if (col < fieldWidth - 1)
+                    {
+                        battleField[1, col] = "-";
+                    }
+
+                    battleField[row, 0] = Convert.ToString(row - 2);
+                    battleField[row, 1] = "|";
+                    if (col % 2 == 0)
+                    {
+                        battleField[row, col] = "-";
+                    }
+                    else
+                    {
+                        battleField[row, col] = " ";
+                    }
+                }
+            }
+
+            Methods.FillingTheArray(battleFieldSize, fieldHeight, fieldWidth, battleField);
+            Methods.PrintArray(fieldHeight, fieldWidth, battleField);
+            int countPlayed = 0;
+            Methods.VremeEIgrachaDaDeistva(battleFieldSize, fieldHeight, fieldWidth, battleField, countPlayed);
+        }
+
         public static void FillingTheArray(int n, int rows, int cols, string[,] workField)
         {
             int count = 0;
@@ -52,7 +118,7 @@
             }
         }
 
-        public static void VremeEIgrachaDaDeistva(int n, int rows, int cols, string[,] workField, int countPlayed)
+        public static void PlayerTurn(int n, int rows, int cols, string[,] workField, int countPlayed)
         {
             countPlayed++;
             Console.WriteLine("Please enter coordinates: ");
@@ -121,7 +187,7 @@
             PrintArray(rows, cols, workField);
             if (!Krai(rows, cols, workField))
             {
-                VremeEIgrachaDaDeistva(n, rows, cols, workField, countPlayed);
+                PlayerTurn(n, rows, cols, workField, countPlayed);
             }
             else
             {
