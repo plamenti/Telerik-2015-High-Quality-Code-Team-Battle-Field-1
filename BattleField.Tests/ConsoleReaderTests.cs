6eed7458d121e77be2e81d07fb1d05f9178795f2
plamenti <plamenti_@abv.bg>
2015-10-07 23:38:45 +0300

    Installed JustMock Created ConsoleReaderTests.cs Added test for ReadSingleNumber method using mocking

---
+++ BattleField.Tests/ConsoleReaderTests.cs
@@ -0,0 +1,52 @@
+﻿namespace BattleField.Tests
+{
+    using BattleFieldGame.Contracts;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Telerik.JustMock;
+
+    [TestClass]
+    public class ConsoleReaderTests
+    {
+        [TestMethod]
+        public void TestReadSingleNumberMethodShouldReturnIntegerParsedFromString()
+        {
+            string numberToParse = "1";
+            int expected = 1;
+            int actual = 0;
+            var fakeConsoleReader = Mock.Create<IReader>();
+            Mock.Arrange(() => fakeConsoleReader.ReadSingleNumber()).DoInstead(() =>
+            {
+                int.TryParse(numberToParse, out actual);
+            });
+
+            fakeConsoleReader.ReadSingleNumber();
+
+            Assert.AreEqual(expected, actual, "ReadSingleNumber method should return 1 that is parsed from string.");
+        }
+
+        [TestMethod]
+        public void TestReadSingleNumberMethodShouldReturnStringDescribingWhileCycleIfTryParseReturnsFalse()
+        {
+            string numberToParse = "Not a number";
+            int parsedNumber;
+            string expected = "Enter a number between 0 and 5!";
+            string actual = "";
+            var fakeConsoleReader = Mock.Create<IReader>();
+            Mock.Arrange(() => fakeConsoleReader.ReadSingleNumber()).DoInstead(() =>
+            {
+                if (!int.TryParse(numberToParse, out parsedNumber))
+                {
+                    actual = "Enter a number between 0 and 5!";
+                }
+                else
+                {
+                    actual = "Success";
+                }
+            });
+
+            fakeConsoleReader.ReadSingleNumber();
+
+            Assert.AreEqual(expected, actual, "ReadSingleNumber method should return warning message if number to parse is invalid string.");
+        }
+    }
+}
