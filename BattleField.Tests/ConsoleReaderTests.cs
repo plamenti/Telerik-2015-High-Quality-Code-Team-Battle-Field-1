6c26ea30716de1e655aac6031e3bc250632ffc5c
plamenti <plamenti_@abv.bg>
2015-10-08 23:54:32 +0300

    Added test for ReadCoodinates method using mocking

--- BattleField.Tests/ConsoleReaderTests.cs
+++ BattleField.Tests/ConsoleReaderTests.cs
@@ -48,5 +48,67 @@
 
             Assert.AreEqual(expected, actual, "ReadSingleNumber method should return warning message if number to parse is invalid string.");
         }
+
+        [TestMethod]
+        public void TestReadCoordinatesMethodShouldReturnSrtingDescribingWhileInputLEngthIsNotEqualToThree()
+        {
+            string input = "12";
+            string expected = "Please enter coordinates/row, col/ stared from zero(0), separated by whitespace: ";
+            string actual = "";
+            var fakeConsoleReader = Mock.Create<IReader>();
+            Mock.Arrange(() => fakeConsoleReader.ReadCoordinates()).DoInstead(() =>
+            {
+                if (input.Length != 3)
+                {
+                    actual = "Please enter coordinates/row, col/ stared from zero(0), separated by whitespace: ";
+                }
+                else
+                {
+                    actual = "Success";
+                }
+            });
+
+            fakeConsoleReader.ReadCoordinates();
+
+            Assert.AreEqual(expected, actual, "ReadCoordinates Method should return warning message if length of the input is not equal to 3");
+        }
+
+        [TestMethod]
+        public void TestReadCoordinatesMethodShouldReturnArrayContainingTwoIntegersParsedFromInput()
+        {
+            string input = "1 2";
+            int[] expectedArray = { 1, 2 };
+            int[] actualArray = new int[] { 0, 0 };
+            var fakeConsoleReader = Mock.Create<IReader>();
+            Mock.Arrange(() => fakeConsoleReader.ReadCoordinates()).DoInstead(() =>
+            {
+                actualArray[0] = int.Parse(input[0].ToString());
+                actualArray[1] = int.Parse(input[2].ToString());
+            });
+
+            fakeConsoleReader.ReadCoordinates();
+
+            Assert.AreEqual(expectedArray[0], actualArray[0], "First arguments should be equal");
+            Assert.AreEqual(expectedArray[1], actualArray[1], "Second arguments should be equal");
+        }
+
+        public void TestReadCoordinatesMethodShouldReturnArrayWithLength2()
+        {
+            string input = "1 2";
+            int expectedArrayLength = 2;
+            int[] actualArray = new int[] { 0, 0 };
+            int actualArrayLength = 0;
+            var fakeConsoleReader = Mock.Create<IReader>();
+            Mock.Arrange(() => fakeConsoleReader.ReadCoordinates()).DoInstead(() =>
+            {
+                actualArray[0] = int.Parse(input[0].ToString());
+                actualArray[1] = int.Parse(input[2].ToString());
+                actualArrayLength = actualArray.Length;
+            });
+
+            fakeConsoleReader.ReadCoordinates();
+
+            Assert.AreEqual(expectedArrayLength, actualArrayLength, "Array length should be 2");
+        }
     }
 }
