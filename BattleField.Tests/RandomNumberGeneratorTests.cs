e924a6271361b793a9ece14e3d76699309848f5c
plamenti <plamenti_@abv.bg>
2015-10-11 15:24:54 +0300

    Added RandomNumberGeneratorTests

---
+++ BattleField.Tests/RandomNumberGeneratorTests.cs
@@ -0,0 +1,33 @@
+﻿using BattleFieldGame;
+
+namespace BattleField.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class RandomNumberGeneratorTests
+    {
+        [TestMethod]
+        public void TestNextMethodShouldReturnNumberBetweenMinAndMax()
+        {
+            int checkCount = 10;
+            int min = 1;
+            int max = 10;
+            bool actual = true;
+
+            RandomNumberGenerator generator = new RandomNumberGenerator();
+            for (int i = 0; i < checkCount; i++)
+            {
+                var generatedNumber = generator.Next(min, max);
+                bool result = (min <= generatedNumber && generatedNumber <= max);
+                if (result == false)
+                {
+                    actual = false;
+                    break;
+                }
+            }
+
+            Assert.IsTrue(actual);
+        }
+    }
+}
